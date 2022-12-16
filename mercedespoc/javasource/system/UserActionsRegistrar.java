00a51676ff6fbb90b165b7daa821475ee6d83752
jjbreis <jjbreis000@gmail.com>
2022-12-16 14:08:25 +0000

    this

---
+++ mercedespoc/javasource/system/UserActionsRegistrar.java
@@ -0,0 +1,21 @@
+package system;
+
+import com.mendix.core.actionmanagement.IActionRegistrator;
+
+public class UserActionsRegistrar
+{
+  public void registerActions(IActionRegistrator registrator)
+  {
+    registrator.bundleComponentLoaded();
+    registrator.registerUserAction(excelimporter.actions.GetHeaderInformationFromExcelFile.class);
+    registrator.registerUserAction(excelimporter.actions.RefreshClass.class);
+    registrator.registerUserAction(excelimporter.actions.StartImportByTemplate.class);
+    registrator.registerUserAction(mxmodelreflection.actions.ReplaceToken.class);
+    registrator.registerUserAction(mxmodelreflection.actions.SyncObjects.class);
+    registrator.registerUserAction(mxmodelreflection.actions.TestThePattern.class);
+    registrator.registerUserAction(mxmodelreflection.actions.ValidateTokensInMessage.class);
+    registrator.registerUserAction(seleniummodule.actions.FetchFromAFAB.class);
+    registrator.registerUserAction(seleniummodule.actions.SubmitIHKDraft.class);
+    registrator.registerUserAction(system.actions.VerifyPassword.class);
+  }
+}
